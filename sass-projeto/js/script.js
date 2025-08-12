32727b5cf565efad02ada6e8c624f5abf59b840d
MariaJuliaFelix <mariajuliaf160@gmail.com>
2025-08-12 12:08:09 -0300

    updates

--- sass-projeto/js/script.js
+++ sass-projeto/js/script.js
@@ -7,3 +7,20 @@ setInterval(() => {
   index = (index + 1) % imagens.length;
   bannerImg.src = imagens[index];
 }, 5000); 
+
+
+// irei apenas usar GET, POST e PATCH
+
+
+
+fetch("https://d41d75f43ca7.ngrok-free.app/restaurant/menu", {
+    method: 'GET',
+    headers: {
+        'ngrok-skip-browser-warning': true, //acesso no servidor local
+        'Content-Type': 'application/json' //tipo de dado que vou receber
+        }
+      
+})
+.then(response => response)
+.then(response => resolve(response.json())) //extrai o json
+.catch(err => console.log(err)); // erros serao mostrados no console, fins de debug
