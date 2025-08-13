b41c128937ec68c49b39b32b857259933976ba6a
MariaJuliaFelix <mariajuliaf160@gmail.com>
2025-08-13 11:30:54 -0300

    criando a function de aparecer os detalhes do item

--- sass-projeto/js/script.js
+++ sass-projeto/js/script.js
@@ -22,22 +22,46 @@ async function getMenu() {
             }
         });
 
-        const data = await response.json();
+        const data = await response.json(); //extrai o json
         renderMenu(data);
     } catch (err) {
-        console.error(err);
+        console.error(err); //conseguir ver o error no console
     }
 }
 
 function renderMenu(items) {
     const containerMenu = document.querySelector("#container_menu");
 
-    containerMenu.innerHTML = items.map(item => `
+    containerMenu.innerHTML = items.map(item => //.map é pra transformar todos os itens em array
+        ` 
         <div class="card-menu">
             <img src="${item.url_banner}" alt="${item.description}">
-            <p>${item.description}</p>
+            <p class="descricao">${item.description}</p>
+            <p class="valor">R$ ${item.price}</p>
+            <button class="button-comprar" id="button-info">Comprar</button>
         </div>
-    `).join("");
+    `).join(""); //.join junta os pedaços de html pra ficarem um unico texto
 }
 
 getMenu();
+
+async function getInformation() {
+    try {
+        const response = await fetch("https://d41d75f43ca7.ngrok-free.app/restaurant/product/{id}", {
+            method: 'GET',
+            headers: {
+                'ngrok-skip-browser-warning': true,
+                'Content-Type': 'application/json'
+            }
+        });
+
+        document.getElementById("button-info").addEventListener("click", function() {
+            const response = await fetch("https://d41d75f43ca7.ngrok-free.app/restaurant/product/{id}");
+        });
+
+        const data = await response.json(); 
+        renderMenu(data);
+    } catch (err) {
+        console.error(err);
+    }
+}
