b8c37baa2bf11111dcc901dd9e0e8c8d4d18fb4c
MariaJuliaFelix <mariajuliaf160@gmail.com>
2025-08-18 22:36:07 -0300

    refacture

--- sass-projeto/js/buttonInformation.js
+++ sass-projeto/js/buttonInformation.js
@@ -1,17 +1,17 @@
 import { price } from "./services.js";
-
+import { showLoading, hideLoading } from "./loading.js";
 export function buttonInformations() {
   const buttons = document.querySelectorAll(".button-comprar");
   const modal = document.getElementById("meuModal");
   const span = modal.querySelector(".fechar");   
   const modalBody = modal.querySelector("#modal-body"); 
 
-
+  
   buttons.forEach(button => {
     button.addEventListener("click", async function () {
       const id = this.dataset.id;
       console.log("Clicou no botão com id:", id);
-
+      showLoading();
       try {
         const response = await fetch(`https://d41d75f43ca7.ngrok-free.app/restaurant/product/${id}`, {
           method: 'GET',
@@ -28,52 +28,54 @@ export function buttonInformations() {
         const data = await response.json();
         console.log("Dados recebidos:", data);
 
-        modalBody.innerHTML = `
+
+     modalBody.innerHTML = `
           <h2>${data.description}</h2>
           <img class="img-information" src="${data.url_banner}" alt="${data.description}">
           <h3>${price(data.price)}</h3>
-         <div class="select-lista">
-            <select id="envio">
+
+          <div class="forma_de_pagamento">
+            <h2>Forma de pagamento</h2>
+            
+          <div class="cartao">
+          <input id="credit_card_name" placeholder="Nome no cartão" type="text">
+          <input id="credit_card_number" placeholder="Número do cartão" type="text">
+          <input id="credit_card_date_expiration" placeholder="Validade (MM/AA)" type="text">
+          <input id="credit_card_code" placeholder="CVV" type="password">
+        </div>
+
+            <div class="select-lista">
+              <select id="payment_method">
                 <option value="">Escolha o método de envio</option>
-                <option value="delivery">Entrega</option>
-                <option value="establishment">Retirada</option>
-            </select>
+                <option value="delivery">Entrega do pedido</option>
+                <option value="establishment">Retirada do pedido</option>
+              </select>
             </div>
-            <div class="forma_de_pagamento">
-              <h2> Forma do pagamento</h2>
-              <div class="cartao>
-              <input id="credit_card_name"></input>
-              <input id="credit_card_number"></input>
-              <input id="credit_card_date_expiration"></input>
-              <input id="credit_card_code"></input>
-              </div> 
-              
-            <select class="select-lista">
-              <option value="delivery">Entrega</option>
-              <option value="establishment">Retirada</option>
-              </select> 
-            </div> 
+          </div>
 
-        `;
+          <button id="confirmar-pedido" class="button-finalizar">Confirmar Pedido</button>
+`;
 
-        modal.style.display = "flex"; 
+      modal.style.display = "flex";
+      document.body.style.overflow = "hidden";
 
       } catch (erro) {
         console.error("Erro no fetch:", erro);
-      }
+      } finally {
+      hideLoading(); 
+    }
     });
   });
 
-        modal.style.display = "none"; 
+ span.onclick = () => {
+  modal.style.display = "none";
+  document.body.style.overflow = ""; 
+};
 
-
-  span.onclick = () => {
+window.onclick = (event) => {  
+  if (event.target === modal) {
     modal.style.display = "none";
-  };
-
-  window.onclick = (event) => {  
-    if (event.target === modal) {
-      modal.style.display = "none";
-    }
-  };
+    document.body.style.overflow = ""; 
+  }
+};
 }
