788c01d6e1f31d724085a1e9e07f3d87e0273791
MariaJuliaFelix <mariajuliaf160@gmail.com>
2025-08-28 15:30:56 -0300

    refacture

--- sass-projeto/js/renderMenu.js
+++ sass-projeto/js/renderMenu.js
@@ -1,13 +1,25 @@
 import { price } from "./services.js";
+import { confirm, monitorarEntrega } from "./confirm.js";
 
 export function renderMenu(items) {
-    const containerMenu = document.querySelector("#container_menu");
-    containerMenu.innerHTML = items.map(item =>
-        `<div class="card-menu">
-            <img src="${item.url_banner}" alt="${item.description}">
-            <p class="descricao">${item.description}</p>
-           <p class="valor">${price(item.price)}</p>
-            <button class="button-comprar" data-id="${item.id}">Comprar</button>
-        </div>`
-    ).join("");
+  const containerMenu = document.querySelector("#container_menu");
+
+  containerMenu.innerHTML = items.map(item =>
+    `<div class="card-menu" id="card-${item.id}">
+        <img src="${item.url_banner}" alt="${item.description}">
+        <p class="descricao">${item.description}</p>
+        <p class="valor">${price(item.price)}</p>
+        <button class="button-comprar" data-id="${item.id}">Comprar</button>
+        <p class="status-entrega" id="status-${item.id}"></p>
+    </div>`
+  ).join("");
+
+  containerMenu.querySelectorAll(".button-comprar").forEach(button => {
+    button.addEventListener("click", () => {
+      const itemId = button.dataset.id;
+      confirm(itemId); 
+    });
+  });
+
+  items.forEach(item => monitorarEntrega(item.id));
 }
